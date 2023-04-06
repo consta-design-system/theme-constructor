f0ec73d312398277f50f2a869cec97da55a8351a
gizeasy <gizeasy@gmail.com>
2023-04-06 23:18:17 +0300

    chore: add deploy action

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -5,33 +5,8 @@ import description from './description.mdx';
 
 export const { createStand, createPage } = createConfig({
   title: 'Документация дизайн-системы',
-  id: 'portal',
-  groups: [
-    {
-      title: 'О дизайн-системе',
-      id: 'about',
-    },
-    {
-      title: 'Тематизация',
-      id: 'theme',
-    },
-    {
-      title: 'Как работать с Consta',
-      id: 'workflow',
-    },
-    {
-      title: 'Кастомизация',
-      id: 'custom',
-    },
-    {
-      title: 'Портал',
-      id: 'portal',
-    },
-    {
-      title: 'Правила и справочники',
-      id: 'reference',
-    },
-  ],
+  id: 'theme-constuctor',
+  groups: [],
   group: 'Общая документация',
   image,
   description,
